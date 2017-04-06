73048f96e66888f4c72db8f4a43b8e4adfd206fd
Kabir Cahill <kabir_cahill@hotmail.com>
2017-04-06 13:35:18 -0700

    Generates a dynamic maze using recursive backtracking.

--- javascript/gameplay.js
+++ javascript/gameplay.js
@@ -1,6 +1,17 @@
 /**
  * Created by Kabir on 4/4/2017.
  */
+
+N = 1
+S = 2
+E = 4
+W = 8
+dirs = ['N', 'S', 'E', 'W']
+dirsValue = {N : N, S : S, E : E, W : W}
+DX = {E : 1, W : -1, N : 0, S : 0}
+DY = {E : 0, W : 0, N : -1, S : 1}
+OPPOSITE = {E : W, W : E, N : S, S : N}
+
 var gameBoard;
 var context;
 var circle;
@@ -8,11 +19,28 @@ var start;
 var end;
 var horizontalWalls;
 var verticalWalls;
+var grid;
+var height;
+var width;
 
 $(document).ready(function(){
     gameBoard = document.getElementById("gameBoard");
     context = gameBoard.getContext("2d");
     context.translate(0.5, 0.5);
+
+    height = 10;
+    width = 20;
+
+    grid = new Array(height);
+    for(var i = 0; i <grid.length; ++i) {
+        grid[i] = new Array(width);
+
+        for(var j = 0; j < grid[i].length; ++j) {
+            grid[i][j] = 0;
+        }
+    }
+
+    carve_passages_from(0, 0, grid);
 
     circle = {
         radius: 10,
@@ -30,8 +58,6 @@ $(document).ready(function(){
         y: gameBoard.height / 10 * 5,
         color: "#ffff00"
     }
-
-    buildMaze();
 
     drawGrid();
     drawEnd();
@@ -57,66 +83,71 @@ function drawGrid() {
     for(var i = gameBoard.height / 10; i < gameBoard.height; i += gameBoard.height / 10) {
         context.moveTo(0, i);
         context.lineTo(gameBoard.width, i);
-
     }
 
     context.stroke();
 }
 
-function buildMaze() {
-    horizontalWalls = [
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
-        [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-        [0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-        [0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
-        [0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0]
-    ];
+/* ------------------------------------------------------------ */
+function carve_passages_from(cx, cy, grid) {
+    var directions = shuffle(dirs)
 
-    verticalWalls = [
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
-        [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
-        [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1],
-        [1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]
-    ];
+    directions.forEach(function(direction) {
+        var nx = cx + DX[direction]
+        var ny = cy + DY[direction]
+
+        if (ny >= 0 && ny < grid.length && nx >= 0
+            && nx < grid[0].length && grid[ny][nx] === 0) {
+            grid[cy][cx] += dirsValue[direction]
+            grid[ny][nx] += OPPOSITE[direction]
+            carve_passages_from(nx, ny, grid)
+        }
+    })
 }
+
+function shuffle(o){
+    for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+};
 
 function drawMaze() {
     context.strokeStyle = "#000000";
     context.beginPath();
 
-    // build horizontal walls
-    for(var row = 0; row < horizontalWalls.length; ++row) {
-        context.moveTo(0, (row + 1) * gameBoard.height / 10);
+    for(var row = 0; row < grid.length; ++row) {
+        for(var col = 0; col < grid[row].length; ++col) {
+            var val = grid[row][col];
+            var top = row * gameBoard.height / 10;
+            var left = col * gameBoard.width / 20;
+            context.moveTo(left, top);
 
-        for(var col = 0; col < horizontalWalls[0].length; ++col) {
-            if(horizontalWalls[row][col]) {
-                context.lineTo((col + 1) * gameBoard.width / 20, (row + 1) * gameBoard.height / 10);
+            if(val / W >= 1) {
+                val %= W;
             } else {
-                context.moveTo((col + 1) * gameBoard.width / 20,  (row + 1) * gameBoard.height / 10);
+                context.lineTo(left, top + gameBoard.height / 10);
             }
-        }
-    }
 
-    // build vertical walls
-    for(var col = 0; col < verticalWalls[0].length; ++col) {
-        context.moveTo((col + 1) * gameBoard.width / 20, 0);
-
-        for(var row = 0; row < verticalWalls.length; ++row) {
-            if(verticalWalls[row][col]) {
-                context.lineTo((col + 1) * gameBoard.width / 20, (row + 1) * gameBoard.height / 10);
+            if(val / E >= 1) {
+                val %= E;
             } else {
-                context.moveTo((col + 1) * gameBoard.width / 20,  (row + 1) * gameBoard.height / 10);
+                context.moveTo(left + gameBoard.width / 20, top);
+                context.lineTo(left + gameBoard.width / 20, top + gameBoard.height / 10);
             }
+
+            if(val / S >= 1) {
+                val %= S;
+            } else {
+                context.moveTo(left, top + gameBoard.height / 10);
+                context.lineTo(left + gameBoard.width / 20, top + gameBoard.height / 10);
+            }
+
+            if(val < N) {
+                context.moveTo(left, top);
+                context.lineTo(left + gameBoard.width / 20, top);
+            }
+
+            //context.font = "10px Arial";
+            //context.fillText(grid[row][col], left + 15, top + 15);
         }
     }
 
@@ -142,6 +173,8 @@ function moveCircle(e) {
     var rect = gameBoard.getBoundingClientRect();
     circle.x = e.clientX - rect.left;
     circle.y = e.clientY - rect.top;
+    var row = Math.floor(circle.y / (gameBoard.height / 10));
+    var col = Math.floor(circle.x / (gameBoard.width / 20));
 
     // check if circle has reached left or right boundary
     if(circle.x + circle.radius > gameBoard.width) {
@@ -156,6 +189,8 @@ function moveCircle(e) {
     } else if (circle.y - circle.radius < 0) {
         circle.y = circle.radius;
     }
+
+
 
     // if(touchingWall()) {
     //     alert("Oops!");
